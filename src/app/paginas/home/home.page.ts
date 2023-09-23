b52fe5c39f979d087a16edaf97dd96f349475095
Leandro Cabeza <cabezaleandro@gmail.com>
2023-09-23 00:09:35 -0300

    home avanzado

--- src/app/paginas/home/home.page.ts
+++ src/app/paginas/home/home.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  vista: string = "colores";
+  idioma: string = "espanol";
+
   constructor() { }
 
   ngOnInit() {
